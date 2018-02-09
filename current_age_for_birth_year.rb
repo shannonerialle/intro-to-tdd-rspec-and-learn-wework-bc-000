d4d2e2a0d4ea4f936f673f4e452843f3f7eba315
Shannon Ford <shannon.ford@wework.com>
2018-02-09 00:30:49 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,4 @@
 def current_age_for_birth_year
-  age_of_person = current_age_for_birth_year(1984)
-  current_age_for_birth_year {[2003]}
+  current_age_for_birth_year(1984)
+  age_of_person {[2003]}
 end
